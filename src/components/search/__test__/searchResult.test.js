db01ad52436d54e1c2028d36d7ab9f976ec078c6
mkumail1 <mkumailofficial@gmail.com>
2022-04-23 16:59:09 +0500

    changes

--- src/components/search/__test__/searchResult.test.js
+++ src/components/search/__test__/searchResult.test.js
@@ -13,15 +13,3 @@ test('it check if the component is in loading state after the input is called', 
   const res = await screen.findAllByTestId('loading');
   expect(res.length).toBe(1);
 });
-
-// test('check if result card is returned after api is resolved', async () => {
-//   const { rerender } = render(<SearchComponent />);
-//   fireEvent.change(await screen.findByTestId('search'), {
-//     target: { value: 'atif' },
-//   });
-//   await requestSearch('atif').then(async (data) => {
-//     rerender(<ResultComponent results={data} />);
-//     const res = await screen.findAllByTestId('hello');
-//     expect(res.length).toBe(1);
-//   });
-// });
