f42e9a9f0860d4d20d18d8772cbeacf74d4b1cb9
mkumail1 <mkumailofficial@gmail.com>
2022-04-23 17:08:22 +0500

    updates

--- src/components/search/__test__/searchResult.test.js
+++ src/components/search/__test__/searchResult.test.js
@@ -1,9 +1,5 @@
 import SearchComponent from '../searchComponent';
-import { ResultCard, ResultComponent } from '../../result';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { requestSearch } from '../../../util/helper';
-import App from '../../../App';
-import { BrowserRouter, Routers } from 'react-router-dom';
 
 test('it check if the component is in loading state after the input is called', async () => {
   render(<SearchComponent />);
